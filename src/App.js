aee81a62baafec9174969c447a3f5c90996dc242
brandonhuang <hi@brandonhuang.io>
2016-07-27 19:12:09 -0700

    update homepage and readme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Hey there!</h1>
+        <p>You might be wondering what happened to Chatnon! July 25th, was this website's one year anniversary. To celebrate, this entire website will be rewritten from the ground up to be better and more exciting! Feedback is crucial, so if there is anything that bothers you, please yell at me.</p>
+        <p>The rewrite will span multiple months and it will take a while for Chatnon to get back where it was. Check back soon, there will be updates everyday.</p>
+        <p>This entire project is open sourced on <a href="https://github.com/brandonhuang/chatnon-2">Github</a>.</p>
+        <p>- Brandon</p>
       </div>
     );
   }
